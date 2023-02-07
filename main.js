d45cc37f620cd683e964a6aa9a6741f5876b04b2
staylor-165 <63858862+staylor-165@users.noreply.github.com>
2023-02-06 22:10:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+window.onload = function() {
+  console.log( 'testing' )
+}
 
+window.onload = function() {
+  // get HTML element
+  boxElement = document.querySelector( 'a-box' )
+  // get underlying JS 3d object
+  box = boxElement.object3D
+  // change rotation, expressed in radians
+  box.rotation.x = Math.PI / 4
+}
+
+window.onload = function() {
+  boxElement = document.querySelector( 'a-box' )
+  box = boxElement.object3D
+  
+  // create our animation function
+  animate = function() {
+    box.rotation.x += .01
+    window.requestAnimationFrame( animate )
+  }
+  // call our newly made function
+  animate()
+}
